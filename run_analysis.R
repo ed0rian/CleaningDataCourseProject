67d2be2c83590ae0d10568e4cac112586f9becbd
ed0rian <ed0rian@users.noreply.github.com>
2015-01-22 13:33:13 +0100

    Clipped data to have only the things needed.

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,35 @@
 library(data.table)
 
-featureLabels  <- fread("features.txt", header = FALSE, drop = c(1))[[1]]
-activityLabels <- fread("activity_labels.txt", header = FALSE, drop = c(1))[[1]]
+featureLabels   <- fread("features.txt", header = FALSE, drop = c(1))[[1]]
+labelsSelectedN <- c(grep("mean()", featureLabels), grep("std()", featureLabels))
+labelsSelectedN <- sort(unique(labelsSelectedN))
+labelsSelected  <- featureLabels[labelsSelectedN]
 
-testSubjects   <- fread("test/subject_test.txt", header = FALSE)[[1]]
-testActivity   <- fread("test/y_test.txt", header = FALSE)[[1]]
-testData       <- read.table("test/X_test.txt")
+activityLabels  <- fread("activity_labels.txt", header = FALSE, drop = c(1))[[1]]
 
-testSelect <- c(grep("mean()", featureLabels), grep("std()", featureLabels))
-testSelect <- sort(unique(testSelect))
+trainSubjects   <- fread("train/subject_train.txt", header = FALSE)[[1]]
+trainActivity   <- fread("train/y_train.txt", header = FALSE)[[1]]
+trainData       <- read.table("train/X_train.txt")
+trainData       <- data.table(trainData)
+
+testSubjects    <- fread("test/subject_test.txt", header = FALSE)[[1]]
+testActivity    <- fread("test/y_test.txt", header = FALSE)[[1]]
+testData        <- read.table("test/X_test.txt")
+testData        <- data.table(testData)
+
+trainSelected   <- trainData[, labelsSelectedN, with = FALSE]
+testSelected    <- testData[, labelsSelectedN, with = FALSE]
+workData        <- rbindlist(list(trainSelected, testSelected))
+subjects        <- c(trainSubjects, testSubjects)
+activity        <- c(trainActivity, testActivity)
+
+rm(testSelected)
+rm(testData)
+rm(testActivity)
+rm(testSubjects)
+rm(trainSelected)
+rm(trainData)
+rm(trainActivity)
+rm(trainSubjects)
+rm(featureLabels)
+
