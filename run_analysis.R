ddf951f926f743835a151a5ec8b9abe13e1c378e
ed0rian <ed0rian@users.noreply.github.com>
2015-01-22 14:44:34 +0100

    Tasks 1-4 done

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,21 @@
 library(data.table)
 
 featureLabels   <- fread("features.txt", header = FALSE, drop = c(1))[[1]]
-labelsSelectedN <- c(grep("mean()", featureLabels), grep("std()", featureLabels))
+labelsSelectedN <- c(grep("mean()", featureLabels, fixed = TRUE), 
+                     grep("std()", featureLabels, fixed = TRUE)
+                   )
 labelsSelectedN <- sort(unique(labelsSelectedN))
 labelsSelected  <- featureLabels[labelsSelectedN]
 
 activityLabels  <- fread("activity_labels.txt", header = FALSE, drop = c(1))[[1]]
 
 trainSubjects   <- fread("train/subject_train.txt", header = FALSE)[[1]]
-trainActivity   <- fread("train/y_train.txt", header = FALSE)[[1]]
+trainActivityN  <- fread("train/y_train.txt", header = FALSE)[[1]]
 trainData       <- read.table("train/X_train.txt")
 trainData       <- data.table(trainData)
 
 testSubjects    <- fread("test/subject_test.txt", header = FALSE)[[1]]
-testActivity    <- fread("test/y_test.txt", header = FALSE)[[1]]
+testActivityN   <- fread("test/y_test.txt", header = FALSE)[[1]]
 testData        <- read.table("test/X_test.txt")
 testData        <- data.table(testData)
 
@@ -21,15 +23,23 @@ trainSelected   <- trainData[, labelsSelectedN, with = FALSE]
 testSelected    <- testData[, labelsSelectedN, with = FALSE]
 workData        <- rbindlist(list(trainSelected, testSelected))
 subjects        <- c(trainSubjects, testSubjects)
-activity        <- c(trainActivity, testActivity)
+activityN       <- c(trainActivityN, testActivityN)
 
 rm(testSelected)
 rm(testData)
-rm(testActivity)
+rm(testActivityN)
 rm(testSubjects)
 rm(trainSelected)
 rm(trainData)
-rm(trainActivity)
+rm(trainActivityN)
 rm(trainSubjects)
 rm(featureLabels)
 
+returnActivity <- function(x) {
+    activityLabels[x]
+}
+activity <- sapply(activityN, returnActivity)
+labels   <-c(labelsSelected, "Activity", "Subject")
+workData <- cbind(workData, activity, subjects)
+
+setnames(workData, labels)
